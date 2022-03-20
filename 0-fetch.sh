aa07862f793aa318429adc418909dcdb0b78a626
Ella Stanforth <ella@iglunix.org>
2022-03-20 09:01:13 +0000

    fix fetch

--- 0-fetch.sh
+++ 0-fetch.sh
@@ -5,8 +5,8 @@ echo
 echo '>>> fetching'
 echo
 
-# curl "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KERN_VER.tar.xz" -o $SOURCES/linux-$KERN_VER.tar.xz
-# curl "https://musl.libc.org/releases/musl-$MUSL_VER.tar.gz" -o $SOURCES/musl-$MUSL_VER.tar.gz
+curl "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KERN_VER.tar.xz" -o $SOURCES/linux-$KERN_VER.tar.xz
+curl "https://musl.libc.org/releases/musl-$MUSL_VER.tar.gz" -o $SOURCES/musl-$MUSL_VER.tar.gz
 curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VER/llvm-project-$LLVM_VER.src.tar.xz" -o $SOURCES/llvm-$LLVM_VER.tar.xz
 
 tar -xf $SOURCES/linux-$KERN_VER.tar.xz -C $SOURCES
