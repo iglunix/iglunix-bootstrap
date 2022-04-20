9edcafb0e8ac017c917a9dce99df751d508fef5d
Ella Stanforth <ella@iglunix.org>
2022-04-20 10:09:03 +0000

    wrappers: use -Wno-unused-command-line-argument

--- x86_64-iglunix-linux-musl-c++.sh
+++ x86_64-iglunix-linux-musl-c++.sh
@@ -3,4 +3,5 @@ SYSROOT=$(realpath $(dirname $0))/sysroot/
 clang++ --sysroot=$SYSROOT --target=x86_64-linux-musl \
 -nostdinc++ -isystem $SYSROOT/include/c++/v1 \
 -rtlib=compiler-rt -stdlib=libc++ -unwindlib=libunwind \
+-Wno-unused-command-line-argument \
 "$@"
--- x86_64-iglunix-linux-musl-cc.sh
+++ x86_64-iglunix-linux-musl-cc.sh
@@ -2,4 +2,5 @@
 SYSROOT=$(realpath $(dirname $0))/sysroot/
 clang --sysroot=$SYSROOT --target=x86_64-linux-musl \
 -rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ \
+-Wno-unused-command-line-argument \
 "$@"
