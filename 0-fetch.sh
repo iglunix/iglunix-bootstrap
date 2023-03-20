14baf20f4e89dd2b4cb1b4ef3a3c3ce688e89cd9
Ella Stanforth <ella@iglunix.org>
2023-03-20 13:30:58 +0000

    fix mksh source path

--- 0-fetch.sh
+++ 0-fetch.sh
@@ -8,7 +8,7 @@ echo
 curl "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERN_VER.tar.xz" -o $SOURCES/linux-$KERN_VER.tar.xz
 curl "https://musl.libc.org/releases/musl-$MUSL_VER.tar.gz" -o $SOURCES/musl-$MUSL_VER.tar.gz
 curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VER/llvm-project-$LLVM_VER.src.tar.xz" -o $SOURCES/llvm-$LLVM_VER.tar.xz
-curl -LO "http://www.mirbsd.org/MirOS/dist/mir/mksh/mksh-$MKSH_VER.tgz"
+curl -L "http://www.mirbsd.org/MirOS/dist/mir/mksh/mksh-$MKSH_VER.tgz" -o $SOURCES/mksh-$MKSH_VER.tgz
 
 tar -xf $SOURCES/linux-$KERN_VER.tar.xz -C $SOURCES
 tar -xf $SOURCES/musl-$MUSL_VER.tar.gz -C $SOURCES
